0a2cf4044ea3c15c35a494d2a978c38e967467b3
Alex <areichel1986@gmail.com>
2023-08-29 11:34:36 +0200

    charactersonthefront

--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -1,7 +1,35 @@
-export default function Character(){
-    return (
-        <>
-           
-        </>
-    )
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+
+export default function CharacterList() {
+  const router = useRouter();
+  const { data } = useSWR("/api/characters");
+
+  if (!data) {
+    return <h1>Loading...</h1>;
+  }
+  console.log("mydata_in_characters",data);
+  return (
+    <>
+      
+      
+        {data.map((character) => (
+          <li key={character._id}>
+            {/* <StyledButtonBUtton type="button" onClick={() => router.push(`/${character._id}`)}> */}
+              {character.name}
+              {/* {character.attack}
+              {character.comparison}
+              {character.description}
+              {character.leavingbehindtrail}
+              {character.weaknesses}
+              {character.zodiac} */}
+
+              {/* </StyledButtonBUtton> */}
+
+          </li>
+        ))}
+     
+    </>
+  );
 }
