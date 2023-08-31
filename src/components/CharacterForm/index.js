9fa908ca923d8526f53952c0139d39a3089bc17d
Alex <areichel1986@gmail.com>
2023-08-31 15:02:15 +0200

    yes

--- src/components/CharacterForm/index.js
+++ src/components/CharacterForm/index.js
@@ -19,10 +19,10 @@ export default function CreateCharacterForm() {
     return (
 <div>
 <form onSubmit={onSubmit}>
-      <div className='container mx-auto px-20'>
+      <div className='container mx-auto px-10 mt-10 space-y-10'>
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Profile</h2>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">Create the Character</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
             This information will be displayed publicly so be careful what you share.
           </p>
@@ -76,15 +76,14 @@ export default function CreateCharacterForm() {
             </div>
 
             <div className="sm:col-span-3">
-              <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">
-                Description
+              <label htmlFor="attack" className="block text-sm font-medium leading-6 text-gray-900">
+              Attack
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="description"
-                  id="description"
-                  autoComplete="family-name"
+                  name="attack"
+                  id="attack"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -95,60 +94,57 @@ export default function CreateCharacterForm() {
 
 
             <div className="col-span-full">
-              <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
-                Street address
+              <label htmlFor="comparison" className="block text-sm font-medium leading-6 text-gray-900">
+              Comparison
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="street-address"
-                  id="street-address"
-                  autoComplete="street-address"
+                  name="comparison"
+                  id="comparison"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div className="sm:col-span-2 sm:col-start-1">
-              <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
-                City
+              <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">
+                Descritpion
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="city"
-                  id="city"
-                  autoComplete="address-level2"
+                  name="description"
+                  id="description"
+                
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div className="sm:col-span-2">
-              <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">
-                State / Province
+              <label htmlFor="leavingbehindtrail" className="block text-sm font-medium leading-6 text-gray-900">
+                Leaving no trail behind
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="region"
-                  id="region"
-                  autoComplete="address-level1"
+                  name="leavingbehindtrail"
+                  id="leavingbehindtrail"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
 
             <div className="sm:col-span-2">
-              <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
-                ZIP / Postal code
+              <label htmlFor="zodiac" className="block text-sm font-medium leading-6 text-gray-900">
+                Zodiac
               </label>
               <div className="mt-2">
                 <input
                   type="text"
-                  name="postal-code"
-                  id="postal-code"
-                  autoComplete="postal-code"
+                  name="zodiac"
+                  id="zodiac"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -157,10 +153,9 @@ export default function CreateCharacterForm() {
         </div>
 
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">When everything is fine</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
-            We'll always let you know about important changes, but you pick what else you want to hear about.
-          </p>
+You will see the character          </p>
 
 
         </div>
