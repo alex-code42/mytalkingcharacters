49c8ac54c1ec49015aaac265c8a318c5dae12935
Alex <areichel1986@gmail.com>
2023-09-01 18:17:00 +0200

    loggin

--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { useSession, signOut, getSession } from "next-auth/react";
+import { useSession,signIn, signOut, getSession } from "next-auth/react";
+import Link from "next/link";
+
+const callbackUrl = "/login"
 
 export default function Account() {
   const { data: session, status } = useSession()
@@ -23,8 +26,39 @@ export default function Account() {
   )
 }
 
+export function NavbarAccountCheck() {
+    const { data: session, status } = useSession()
+    if (status=== "authenticated") {
+      return (
+        <>
+        <div>
+              <button onClick={() => signOut()}>{session.user.name} <br></br>Sign out</button>
+          </div>
+        </>
+      )
+    }
+    return (
+      <>
+       <div>
+              <button onClick={() => signIn()}>Sign In</button>
+          </div>
+      </>
+    )
+  }
+
+
+
 export const getServerSideProps = async (context) => {
     const session = await getSession(context)
+
+
+    if (!session){
+        return{
+            redirect:{
+                destination: "/login",
+            }
+        }
+    }
 
     return{
         props: {session},
