58f718b97cdabdd7732ffe4eddcedd7e89e2ac2e
Alex <areichel1986@gmail.com>
2023-09-02 13:19:52 +0200

    s

--- src/components/CharacterForm/index.js
+++ src/components/CharacterForm/index.js
@@ -1,8 +1,9 @@
 import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 import useSWR from "swr";
+import { useRouter } from 'next/router'
 
 export default function Form({onSubmit, formName, defaultData }) {
-
+  const router = useRouter();
   
    
     return (
@@ -136,9 +137,9 @@ export default function Form({onSubmit, formName, defaultData }) {
       </div>
 
       <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-          Cancel
-        </button>
+      <button className="rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" type="button" onClick={() => router.back()}>
+              Cancel
+            </button>
         <button
           type="submit"
           className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
