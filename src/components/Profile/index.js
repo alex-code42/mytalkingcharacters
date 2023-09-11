95432f8da4836f02c9849f0097369762d80efc9d
Alex <areichel1986@gmail.com>
2023-09-11 17:56:06 +0200

    yes

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -64,26 +64,7 @@ export default function MyProfile({session}) {
           <h2 className="text-3xl font-bold tracking-tight text-slate-100 sm:text-4xl">Hi, {session.user.name}</h2>
           <h4 className="text-base font-bold tracking-tight text-slate-100 sm:text-base">Lets have a Chat</h4>
         </div>
-        <ul role="list" className="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2  ">
-          {people.map((person) => (
-            <li key={person.name}>
-              <div className="flex items-center gap-x-6 ">
-              <Image
-                src={mypic}
-                width={500}
-                height={500}
-                quality={65}
-                className=" h-24 w-24 rounded-full mt-8 border mx-auto border-indigo-400 drop-shadow-xl"
-                alt="Picture of the author"
-                /> 
-                <div>
-                  <h3 className="text-base font-semibold leading-7 tracking-tight text-slate-100">{person.name}</h3>
-                  <p className="text-sm font-semibold leading-6 text-indigo-400 ">{person.role}</p>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
+
       </div>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -36,8 +36,7 @@ export default function Home() {
     <Navbar/>
     <HeroSection/>
     <ContentSection1/>
-      
-        <LoginComponent/>
+    <LoginComponent/>
 
     </div>
   );
