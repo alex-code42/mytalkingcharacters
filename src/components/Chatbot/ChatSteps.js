2ec85fb5fd2796cbca07e5517d63bd56c7027a16
Alex <areichel1986@gmail.com>
2023-09-13 12:16:45 +0200

    users-can-chat-with-the-bot

--- src/components/Chatbot/ChatSteps.js
+++ src/components/Chatbot/ChatSteps.js
@@ -1,21 +1,34 @@
 // components/ChatSteps.js
+import { useSession } from "next-auth/react"
+
+
+ 
+
+
+
+
 
 import React from 'react';
 
 const ChatSteps = ({ result }) => {
-  console.log(result);
+  console.log("this is the result--->>>>>>>>>>>",result);
   
   // Check if result is an array before mapping
-  if (!Array.isArray(result)) {
-    return <div>Please type in. I will answer everything.</div>;
-  }
+  
   
   // Split the result into separate steps using newline character '\n'
 //   const steps = result.answer.split('\n');
 // console.log(result)
 
   // Create a preformatted text block to display the steps
- 
+  const { data: session, status } = useSession()
+
+  if (status === "authenticated") {
+
+    if (!Array.isArray(result)) {
+      return <div>Please type in. I will answer everything.</div>;
+    }
+
 
   return (
     <div>
@@ -38,6 +51,23 @@ const ChatSteps = ({ result }) => {
             </li>
         ))}
       </ul>
+    </div>
+  );
+}
+
+if (!result) {
+  return <div>Please type in. I will answer everything.</div>;
+}
+
+
+return (
+    <div>
+    <div className='mb-6'>
+
+            {result.split('\n').map((element)=>(
+              <p className='mb-6'> {element}</p>
+            ))}
+            </div>
     </div>
   );
 };
